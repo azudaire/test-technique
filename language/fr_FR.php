868045da3d37bf3579f3bfbeb03470dbf092d027
andde <andde@andde.(none)>
2013-12-28 12:58:18 +0100

    test-technique

---
+++ language/fr_FR.php
@@ -0,0 +1,28 @@
+<?php
+return array(
+    '__users_list'  =>  'Liste des utilisateurs',
+
+    '__add_user'    =>  'Ajouter un utilisateur',
+    '__remove_user' =>  'Supprimer',
+    '__edit_user'   =>  'Modifier',
+
+    '__user_id' =>  '#',
+    '__user_firstname'  =>  'Prénom',
+    '__user_lastname'   =>  'Nom',
+    '__user_email'  =>  'Adresse email',
+    '__user_date_of_birth' => 'date de naissance',
+    '__user_address' => 'Adresse',
+
+    //Form User
+
+    '__label_user_firstname'         =>  'Prénom',
+    '__label_user_lastname'          =>  'Nom',
+    '__label_user_email'             =>  'Email',
+    '__label_user_password'          =>  'Password',
+    '__label_user_address'            =>  'Adresse',
+    '__label_user_date_of_birth'     =>  'Date de naissance',
+    '__save'                         =>  'Enregistrer',
+
+    //Form messages
+    'Value is required and can\'t be empty' =>  'Ce champ est obligatoire'
+)?>
---
+++ module/Application/src/Application/Controller/UserController.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class UserController extends AbstractActionController
+{
+    public function listAction()
+    {
+
+        $users = $this->getServiceLocator()->get('entity_manager')
+            ->getRepository('Application\Entity\User')
+            ->findAll();
+
+        return new ViewModel(array(
+            'users' =>  $users
+        ));
+    }
+
+    public function addAction()
+    {
+        /* @var $form \Application\Form\UserForm */
+        $form = $this->getServiceLocator()->get('formElementManager')->get('form.user');
+
+        $data = $this->prg();
+
+        if ($data instanceof \Zend\Http\PhpEnvironment\Response) {
+            return $data;
+        }
+
+        if ($data != false) {
+            $form->setData($data);
+            
+            if ($form->isValid()) {
+
+                /* @var $user \Application\Entity\User */
+                $user = $form->getData();
+                $user["profile"]["dateOfBirth"] = $data["dateOfBirth"];
+                $user["profile"]["address"] = $data["address"];
+                
+                /* @var $serviceUser \Application\Service\UserService */
+                $serviceUser = $this->getServiceLocator()->get('application.service.user');
+
+                $serviceUser->saveUser($user);
+
+                $this->redirect()->toRoute('users');
+            }
+        }
+
+        return new ViewModel(array(
+            'form'  =>  $form
+        ));
+    }
+
+    public function removeAction()
+    {
+        $userToRemove = $this->getServiceLocator()->get('entity_manager')
+            ->getRepository('Application\Entity\User')
+            ->find($this->params()->fromRoute('user_id'));
+
+        /* @var $serviceUser \Application\Service\UserService */
+        $serviceUser = $this->getServiceLocator()->get('application.service.user');
+
+        $serviceUser->removeUser($userToRemove);
+
+        $this->redirect()->toRoute('users');
+    }
+
+    public function editAction()
+    {
+        /* @var $form \Application\Form\UserForm */
+        $form = $this->getServiceLocator()->get('formElementManager')->get('form.user');
+
+        $userToEdit = $this->getServiceLocator()->get('entity_manager')
+            ->getRepository('Application\Entity\User')
+            ->find($this->params()->fromRoute('user_id'));
+
+        $form->bind($userToEdit);
+        $form->get('firstname')->setValue($userToEdit->getFirstname());
+        $form->get('lastname')->setValue($userToEdit->getLastname());
+        $form->get('dateOfBirth')->setValue($userToEdit->getDateOfBirth());
+        $form->get('address')->setValue($userToEdit->getAddress());
+
+        $data = $this->prg();
+
+        if ($data instanceof \Zend\Http\PhpEnvironment\Response) {
+            return $data;
+        }
+
+        if ($data != false) {
+            $form->setData($data);
+            if ($form->isValid()) {
+
+                /* @var $user \Application\Entity\User */
+                $user = $form->getData();
+
+                //Save the user
+                /* @var $serviceUser \Application\Service\UserService */
+                $serviceUser = $this->getServiceLocator()->get('application.service.user');
+
+                $serviceUser->saveUser($user);
+
+                $this->redirect()->toRoute('users');
+            }
+        }
+
+        return new ViewModel(array(
+            'form'  =>  $form
+        ));
+    }
+
+}
